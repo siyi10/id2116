3b9359aeb63d0414cbaaa155d0dc98ce020fe991
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-02-21 16:33:14 +0800

    Enter

--- daryl.js
+++ daryl.js
@@ -1,6 +1,5 @@
 <script>
-
-</script>function myFunction() {
+function myFunction() {
     var container = document.getElementsByClassName('container')[0]; 
     container.classList.toggle('dark-mode');
   
---
+++ myFunction.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var container = document.getElementsByClassName('container')[0];
+    container.classList.toggle('dark-mode');
+    var image = document.getElementById('image');
+    image.classList.toggle('hidden');
+}
