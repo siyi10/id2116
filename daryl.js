84feb6d5c62ac693c06c8758fbd2ad6448aa20bf
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-02-21 15:47:49 +0800

    comit

--- daryl.js
+++ daryl.js
@@ -1,8 +1,18 @@
-function myFunction() {
-    var container = document.getElementsByClassName('container')[0]; 
-    container.classList.toggle('dark-mode');
+<script>
 
+</script>function myFunction() {
+    var container = document.getElementsByClassName('container')[0]; // because function returns a list
+    container.classList.toggle('dark-mode');
+    // no need to define a click function here because the button already has one defined
+    // in the HTML already
+    /* element.classList.toggle("dark-mode");$("button").click(function(){
+        $("img").toggleClass("hidden");
+    }); */
+ 
+   
+   // this is another way to refer to an element 
    var image = document.getElementById('image');
    image.classList.toggle('hidden');
  }
+ </script> 
  
