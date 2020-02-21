0274115316a1c2809b39a8b683f4198cecd8782a
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-02-21 18:00:07 +0800

    Enter

--- daryl.js
+++ daryl.js
@@ -1,13 +1,10 @@
-<script>
+
 function myFunction() {
     varcontainer = document.getElementsByClassName('container')[0]; 
     container.classList.toggle('dark-mode');
-  /* element.classList.toggle("dark-mode");$("button").click(function(){
-       $("img").toggleClass("hidden");
-   }); */
 
    var image = document.getElementById('image');
    image.classList.toggle('hidden');
  }
- </script> 
+
  
