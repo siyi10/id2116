335eddcf3c2cd002343cde25628437c0fdcfe456
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-02-21 15:08:00 +0800

    commit

---
+++ daryl.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var container = document.getElementsByClassName('container')[0]; 
+    container.classList.toggle('dark-mode');
+
+   var image = document.getElementById('image');
+   image.classList.toggle('hidden');
+ }
+ 
