afe338bd8f6dede1e1b54ef42207660dca27bc93
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-02-25 11:54:08 +0800

    Enter

--- daryl.js
+++ daryl.js
@@ -1,4 +1,8 @@
 
+var i = 0;
+var txt = 'Roses are red\nViolets are blue\nCoding is awesome\nAnd so are you';
+var speed = 50;
+
 function myFunction() {
     var container = document.getElementsByClassName('container')[0]; 
     container.classList.toggle('dark-mode');
@@ -8,6 +12,12 @@ function myFunction() {
 
    var href = document.getElementById("link");
    href.style.display = 'block';
- }
 
- 
+txt.fontcolor( "white");
+
+  if (i < txt.length) {
+    document.getElementById("firstp").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(myFunction, speed);
+  }
+}
