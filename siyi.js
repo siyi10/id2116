20abc6db2bc1a614571a0606616d51810f987cbf
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-02-28 21:43:07 +0800

    Enter

---
+++ siyi.js
@@ -0,0 +1,23 @@
+
+var i = 0;
+var txt = 'Y2 Industrial Design student who absolutely loves coding!';
+var speed = 50;
+
+function myFunction() {
+    var container = document.getElementsByClassName('container')[0]; 
+    container.classList.toggle('dark-mode');
+
+   var image = document.getElementById('image');
+   image.classList.toggle('hidden');
+
+   var href = document.getElementById("link");
+   href.style.display = 'block';
+
+txt.fontcolor( "white");
+
+  if (i < txt.length) {
+    document.getElementById("firstp1").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(myFunction, speed);
+  }
+}
