d3691355ceb9e28f7907571494ac35d694e68c52
kroses13 <60164742+kroses13@users.noreply.github.com>
2020-03-01 21:47:45 +0800

    Enter

--- myFunction.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    var container = document.getElementsByClassName('container')[0];
-    container.classList.toggle('dark-mode');
-    var image = document.getElementById('image');
-    image.classList.toggle('hidden');
-}
